a0105f1c3fedb4960bbdd4255f2ca25440d16eae
persibappsUnofficial <150852922+persibappsUnofficial@users.noreply.github.com>
2024-06-21 23:10:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+// share link button
+document.addEventListener('DOMContentLoaded', function() {
+    const shareButtons = document.querySelectorAll('.btn-share');
+
+    shareButtons.forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            const platform = button.getAttribute('data-platform');
+            const judulArtikel = document.querySelector('h1').innerText.trim();
+            const urlArtikel = window.location.href;
+
+            let shareUrl = '';
+
+            if (platform === 'facebook') {
+                shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlArtikel)}`;
+            } else if (platform === 'twitter') {
+                shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(judulArtikel)}&url=${encodeURIComponent(urlArtikel)}`;
+            } else if (platform === 'whatsapp') {
+                shareUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(judulArtikel + ' ' + urlArtikel)}`;
+            }
+
+            if (shareUrl !== '') {
+                window.open(shareUrl, '_blank');
+            }
+        });
+    });
+});
+
+// scroll reveal
 ScrollReveal({
     reset: true,
     distance: '80px',
